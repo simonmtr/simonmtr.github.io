471946e320cbf17c1bcbcb3881967caa16d6958d
simonmtr <294simon@googlemail.com>
2022-01-12 21:53:44 +0100

    startet react app

---
+++ projects/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Projects from './projects/Projects'
+
+function App() {
+  return (
+    <div className='App'>
+      <Projects></Projects> 
+    </div>
+  );
+}
+
+export default App;
---
+++ projects/src/projects/Projects.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+import './Projects.css';
+
+export default class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render(){
+        return (
+            <div>
+                simonmtr github
+            </div>
+        )
+    }
+}
+
